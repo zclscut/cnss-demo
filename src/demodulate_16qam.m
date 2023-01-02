9b6a7b50d8d4461038681e5f793b3dc40e542e4a
EESJGong <118050627+EESJGong@users.noreply.github.com>
2023-01-02 17:43:36 +0800

    Add files via upload

--- src/demodulate_16qam.m
+++ src/demodulate_16qam.m
@@ -10,6 +10,19 @@ t2=0:tao/symbol_rate:1/symbol_rate-tao/symbol_rate;
 M = 16;
 y_qam = zeros(4, length(x_qam));
 symbols = length(x_qam)/smooth;  %测试信号长度
+Fs=fs;
+wp=2*Fs*tan(2*pi*fp1/(2*Fs)); %通带边界频率
+ws=2*Fs*tan(2*pi*fs1/(2*Fs)); %阻带边界频率
+[n,wn]=buttord(wp,ws,rp,rs,'s'); %滤波器的阶数n和-3dB归一化截止频率Wn
+[b,a]=butter(n,wn,'s');
+[num,den]=bilinear(b,a,Fs);  %双线性变换
+%     [h,w]=freqz(num,den,100,Fs);
+%     figure(1)
+%     plot(w,abs(h));
+%     xlabel('频率/Hz');
+%     ylabel('幅值');
+%     title('巴特沃斯低通滤波器幅度特性');
+%     grid on;
 for iter = 1 : symbols
     y = x_qam(1, (iter-1)*length(t2)+1:iter*length(t2));
     y_len = length(y);
@@ -19,23 +32,11 @@ for iter = 1 : symbols
     Q = -y.*sin(w_qam*t2)*2;
     
     %设计低通滤波器
-    Fs=fs;
-    
-    wp=2*Fs*tan(2*pi*fp1/(2*Fs)); %通带边界频率
-    ws=2*Fs*tan(2*pi*fs1/(2*Fs)); %阻带边界频率
-    [n,wn]=buttord(wp,ws,rp,rs,'s'); %滤波器的阶数n和-3dB归一化截止频率Wn
-    [b,a]=butter(n,wn,'s');
-    [num,den]=bilinear(b,a,Fs);  %双线性变换
+
     I=filter(num,den,I);
     Q=filter(num,den,Q);
-    [h,w]=freqz(num,den,100,Fs);
-    figure(1)
-    plot(w,abs(h));
-    xlabel('频率/Hz');
-    ylabel('幅值');
-    title('巴特沃斯低通滤波器幅度特性');
-    grid on;
-    for i = 1 : y_len
+
+    for i = y_len : y_len
         for j = 1 : M
             distance(j) = sqrt((I(i)-constell_diag(j,1))^2 + (Q(i)-constell_diag(j,2))^2); %接收信号到所有星座点的距离
         end
