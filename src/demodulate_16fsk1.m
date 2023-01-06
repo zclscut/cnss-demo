6af530decb22a2349800c4c612db4540e7f6d49c
EESJGong <118050627+EESJGong@users.noreply.github.com>
2023-01-06 09:53:36 +0800

    16fsk相干解调

--- src/demodulate_16fsk1.m
+++ src/demodulate_16fsk1.m
@@ -1,7 +1,8 @@
 function y_fsk = demodulate_16fsk1(x_fsk,fs, w_fsk, fp1, fs1, rs, rp, smooth, symbol_rate)
-%UNTITLED 此处显示有关此函数的摘要
-%   此处显示详细说明
-% demodulate_16fsk1函数用于对16fsk信号进行相干解调
+% demodulate_16fsk1 用于对16fsk信号进行相干解调
+% x_fsk: 输入的16fsk调制信号; fs: 采样率; w_fsk: 载波信号的角频率; fp1: 低通滤波器通带截止频率; 
+% fs1:低通滤波器阻带截止频率; rs,rp: 滤波器参数; smooth: 单个码元长度; symbol_rate: 码元速率
+
 tao=1/smooth;
 t1=0:tao/symbol_rate:1/symbol_rate-tao/symbol_rate;
 Fs=fs;
@@ -11,13 +12,13 @@ ws=2*Fs*tan(2*pi*fs1/(2*Fs)); %阻带边界频率
 [b,a]=butter(n,wn,'s');
 [num,den]=bilinear(b,a,Fs);  %双线性变换
 y_fsk = zeros(4, length(x_fsk));
-symbols = length(x_fsk)/smooth;  %测试信号长度
+symbols = length(x_fsk)/smooth;  %码元数目
 M = 16;
 for iter = 1 : symbols
     y = x_fsk(1, (iter-1)*length(t1)+1:iter*length(t1));
     y_len = length(y);
     detect = zeros(1,y_len);         % 预置检测信号
-    distance = zeros(1,M);              % 解调：距离检测
+    distance = zeros(1,M);              % 解调：储存该频率下的平均振幅
     for j = 1 : M
         y_ = y.*sin(j*w_fsk*t1);       
         y_=filter(num,den,y_);
