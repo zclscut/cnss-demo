c2d74895d3cc769e2543b3a1ebba6951c851a80e
EESJGong <118050627+EESJGong@users.noreply.github.com>
2023-01-06 09:54:31 +0800

    16qam相干解调

--- src/demodulate_16qam.m
+++ src/demodulate_16qam.m
@@ -1,7 +1,8 @@
 function y_qam = demodulate_16qam(x_qam,fs, w_qam, fp1, fs1, rs, rp, smooth, symbol_rate)
-%UNTITLED2 此处显示有关此函数的摘要
-%   此处显示详细说明
-% demodulate_16qam函数用于16QAM信号的解调
+% demodulate_16qam 用于16QAM信号的解调
+% x_qam: 输入的16qam调制信号; fs: 采样率; w_qam: 载波信号角频率; fp1: 低通滤波器通带截止频率; 
+% fs1:低通滤波器阻带截止频率; rs, rp: 滤波器参数; symbol_rate: 码元速率
+
 constell_diag=[1 1;1 3;1 -1;1 -3;3 1;3 3;3 -1;3 -3;-1 1;-1 3;-1 -1;-1 -3;-3 1;-3 3;-3 -1;-3 -3];
 % %能量归一化,(2,2)归一化模为1
 constell_diag=constell_diag./2/sqrt(2);
@@ -9,20 +10,14 @@ tao=1/smooth;
 t2=0:tao/symbol_rate:1/symbol_rate-tao/symbol_rate;
 M = 16;
 y_qam = zeros(4, length(x_qam));
-symbols = length(x_qam)/smooth;  %测试信号长度
+symbols = length(x_qam)/smooth;  %信号码元数量
 Fs=fs;
 wp=2*Fs*tan(2*pi*fp1/(2*Fs)); %通带边界频率
 ws=2*Fs*tan(2*pi*fs1/(2*Fs)); %阻带边界频率
 [n,wn]=buttord(wp,ws,rp,rs,'s'); %滤波器的阶数n和-3dB归一化截止频率Wn
 [b,a]=butter(n,wn,'s');
 [num,den]=bilinear(b,a,Fs);  %双线性变换
-%     [h,w]=freqz(num,den,100,Fs);
-%     figure(1)
-%     plot(w,abs(h));
-%     xlabel('频率/Hz');
-%     ylabel('幅值');
-%     title('巴特沃斯低通滤波器幅度特性');
-%     grid on;
+
 for iter = 1 : symbols
     y = x_qam(1, (iter-1)*length(t2)+1:iter*length(t2));
     y_len = length(y);
